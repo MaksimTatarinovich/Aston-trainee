dfce70c59dfc6fe7db30617054aafbdba623b5bf
MaksimTatarinovich <makim_21@mail.ru>
2025-02-12 17:08:08 +0300

    don

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,2 +1,38 @@
+import jdk.jfr.Description;
+import lesson_13.Factorial;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import static org.testng.Assert.assertEquals;
+
 public class FactorialTest {
+    Factorial factorial = new Factorial();
+
+    @Test
+    public void testFactorialOfPositiveNumbers() {
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertEquals(factorial.calculateFactorial(1), 1, "Факториал 1 должен быть равен 1");
+        softAssert.assertEquals(factorial.calculateFactorial(5), 120, "Факториал 5 должен быть равен 120");
+        softAssert.assertEquals(factorial.calculateFactorial(12), 479001600, "Факториал 12 должен быть равен 479001600");
+
+        softAssert.assertAll();
+    }
+
+    @Description("Факториал нуля")
+    @Test
+    public void testFactorialOfZero() {
+        assertEquals(1, factorial.calculateFactorial(0), "Факториал 0 должен быть равен 1");
+    }
+
+    @Description("Факториал отрицальтельного числа")
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testFactorialOfNegativeNumber() {
+        factorial.calculateFactorial(-1);
+    }
+
+    @Description("Передача символа")
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testInputChar() {
+        factorial.calculateFactorial('a');
+    }
 }
