bdc4dcde05bd0f9248191f8ca60f2ace299bd4fd
MaksimTatarinovich <makim_21@mail.ru>
2025-02-12 16:33:59 +0300

    add dependency

--- src/main/java/lesson_13/Factorial.java
+++ src/main/java/lesson_13/Factorial.java
@@ -1,4 +1,14 @@
 package lesson_13;
 
 public class Factorial {
+    public long calculateFactorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Невозможно высчитать факториал отрицательного числа");
+        }
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
 }
